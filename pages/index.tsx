c44d237764ca09868f39ec3dd3b619f753152c27
heep-jay <jubrilmuritala69@gmail.com>
2022-07-27 22:50:53 +0100

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Instagram build</div>
+    </div>
+  )
+}
+
+export default Home
