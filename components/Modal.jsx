0bb25994ad99233f960e3930f12e8bb1746479fa
heep-jay <jubrilmuritala69@gmail.com>
2022-07-28 12:45:21 +0100

    Headless UI with Tailwind

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { modalState } from '../atoms/modalAtom'
 import { useRecoilState } from 'recoil';
+import { Dialog, Transition } from '@headlessui/react';
 
 const Modal = () => {
 
   const [open, setOpen] = useRecoilState(modalState);
   return (
-    <div>Modal
+    <Transition.Root show={open}> as={Fragment}
+      <Dialog 
+        as='div'
+        onClose={setOpen} 
+        className='fixed z-10 inset-0 overflow-y-auto'
+      >
+        <div className='flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
+           <Transition.Child 
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0'
+              enterTO='opacity-100'
+              leave='ease-out duration-200'
+              leaveFrom='opacity-100'
+              leaveTO='opacity-0'
 
-      {open && 
-      <p>The Modal is opened</p> }
-    </div>
+           >
+            <Dialog.Overlay className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity'/>
+
+           </Transition.Child>
+           <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden='true'></span>
+        </div>
+      </Dialog>
+    </Transition.Root>
   )
 }
 
