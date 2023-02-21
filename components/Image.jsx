b34826eec8f491538e23dc74e533e3bf924a8d16
heep-jay <jubrilmuritala69@gmail.com>
2023-02-21 12:20:45 -0500

    hshs

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,62 +1,65 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
-import { collection, onSnapshot, orderBy, query, where } from 'firebase/firestore';
-import { db } from '../firebase';
-import { FaComment } from 'react-icons/fa';
-import {  HeartIcon as  HeartIconFilled } from '@heroicons/react/solid';
+import React from "react";
+import { useEffect, useState } from "react";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { FaComment } from "react-icons/fa";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 
+const Image = ({ id, image }) => {
+  const [likes, setLikes] = useState([]);
+  const [comments, setComments] = useState([]);
 
-const Image = ({id, image}) => {
-    const [likes, setLikes] = useState([])
-    const [comments, setComments] = useState([])
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "posts", id, "likes"), (snapshot) => {
+        setLikes(snapshot.docs);
+      }),
+    [db, id]
+  );
 
-    useEffect(
-      () => onSnapshot(
-              collection(db, 'posts', id, 'likes'),
-                (snapshot) => {
-                  setLikes(snapshot.docs)
-                  
-                }),     
-      [db, id]);
-    
-        useEffect(
-          () => onSnapshot (
-                  query(collection(db, 'posts', id, 'comments'), orderBy('timestamp', 'desc')),
-                    (snapshot) => {
-                      setComments(snapshot.docs)
-                      
-                    }),     
-          [db, id]);
-    
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) => {
+          setComments(snapshot.docs);
+        }
+      ),
+    [db, id]
+  );
+
   return (
     <div>
-     <div className='col-span-1 mx-auto my-auto relative'>
-        <img src={image} className=' h-[159px] w-[159px] object-cover bg-black sm:h-[221px] sm:w-[221px]  md:h-[291px] md:w-[291px] lg:h-[293px] lg:w-[293px]  '  alt='' />
-        <div className='h-[159px] w-[159px] absolute text-black z-40 top-0 left-0 bg-black hover:opacity-75 opacity-0 transition-all duration-500 ease-out  object-cover sm:h-[221px] sm:w-[221px]  md:h-[291px] md:w-[291px] lg:h-[293px] lg:w-[293px] '>
-          
-            <div className='absolute top-32 left-24 z-50 flex items-center space-x-3   text-white'>
-              <div className='flex items-center'>
-              <HeartIconFilled  className='w-6 h-6 z-50 mr-2' /><span className='text-sm font-semibold'>{likes.length}</span>
-              </div>
-           <div className='flex items-center'>
-           <FaComment className='w-5 h-5 mr-2'/><span className='text-sm font-semibold'>{comments.length}</span>
-           </div>
-            
-            
-        
+      <div className="col-span-1 mx-auto my-auto relative">
+        <img
+          src={image}
+          className=" h-[159px] w-[159px] object-cover bg-black sm:h-[221px] sm:w-[221px]  md:h-[291px] md:w-[291px] lg:h-[293px] lg:w-[293px]  "
+          alt=""
+        />
+        <div className="h-[159px] w-[159px] absolute text-black z-40 top-0 left-0 bg-black hover:opacity-75 opacity-0 transition-all duration-500 ease-out  object-cover sm:h-[221px] sm:w-[221px]  md:h-[291px] md:w-[291px] lg:h-[293px] lg:w-[293px] ">
+          <div className="absolute top-32 left-24 z-50 flex items-center space-x-3   text-white">
+            <div className="flex items-center">
+              <HeartIconFilled className="w-6 h-6 z-50 mr-2" />
+              <span className="text-sm font-semibold">{likes.length}</span>
+            </div>
+            <div className="flex items-center">
+              <FaComment className="w-5 h-5 mr-2" />
+              <span className="text-sm font-semibold">{comments.length}</span>
+            </div>
           </div>
-        
-            
-    
-           
-          </div>
-          
-          
-
-
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Image
+export default Image;
--- pages/[username].jsx
+++ pages/[username].jsx
@@ -1,97 +1,89 @@
-import React from 'react'
-import { collection, getDocs, onSnapshot, orderBy, query, where } from "firebase/firestore";
-import { db } from '../firebase'
-import { Header, Feed, Modal, ProfileFeed } from '../components';
-import { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { themeState } from '../atoms/themeAtom';
-import Head from 'next/head';
-import { getPosts, getUsername } from '../helpers';
-import { useSession } from 'next-auth/react';
+import React from "react";
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { Header, Feed, Modal, ProfileFeed } from "../components";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { themeState } from "../atoms/themeAtom";
+import Head from "next/head";
+import { getPosts, getUsername } from "../helpers";
+import { useSession } from "next-auth/react";
 
+const UserDetails = ({ userposts, userProfile }) => {
+  const { data: session } = useSession();
+  const [darkMode, setDarkMode] = useState(false);
+  const [mode, setMode] = useState("dark");
+  const [theme, setTheme] = useRecoilState(themeState);
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setMode(window.localStorage.getItem("mode"));
+    }
+  }, [mode, theme]);
 
-const UserDetails = ({userposts, userProfile}) => {
-    const {data: session} = useSession()
-    const [darkMode, setDarkMode] = useState(false);
-    const [mode, setMode] = useState('dark');
-    const [theme, setTheme] = useRecoilState(themeState)
-  
-  
-      useEffect(() => {
-        if (typeof window !== "undefined") {
-  
-           
-            setMode(window.localStorage.getItem('mode'))
-           
-          
-         
-        }
-      
-        
-      }, [mode, theme])
-
-    
   return (
-    <> 
-    {session ? (
-       <div className={`${mode} w-full`}>
-       <div className=" bg-gray-50 dark:bg-black h-screen overflow-y-scroll scrollbar-hide">
-         <Head>
-           <title>`{`Insta App | Profile ${userProfile.username}`}</title>
-           <link rel="icon" href="https://img.icons8.com/color/48/000000/instagram-new--v1.png" />
-         </Head>
-         <Modal/>
-         {/* Header */}
-         <Header/>
-         
-        <ProfileFeed posts={userposts} profile={userProfile}/>
-         
-       </div>
-       </div>
-    ): (<Header/> )}
-   
-    
+    <>
+      {session ? (
+        <div className={`${mode} w-full`}>
+          <div className=" bg-gray-50 dark:bg-black h-screen overflow-y-scroll scrollbar-hide">
+            <Head>
+              <title>`{`Insta App | Profile ${userProfile.username}`}</title>
+              <link
+                rel="icon"
+                href="https://img.icons8.com/color/48/000000/instagram-new--v1.png"
+              />
+            </Head>
+            <Modal />
+            {/* Header */}
+            <Header />
+
+            <ProfileFeed posts={userposts} profile={userProfile} />
+          </div>
+        </div>
+      ) : (
+        <Header />
+      )}
     </>
-        
-    
-  )
-}
+  );
+};
 
 export default UserDetails;
 
 export async function getStaticProps({ params }) {
-  const posts =    await getPosts(params?.username)
-  const username =  await getUsername(params?.username)
-   
-     
- 
-    return {
-      props: {
-        userposts: posts,
-        userProfile: username,
-      }
-    };
-  }
+  console.log(params);
+  const posts = await getPosts(params?.username);
+  const username = await getUsername(params?.username);
 
-  export async function getStaticPaths() {
-    let snapshots = [];
-    let username = ''
-    const postsRef = collection(db, "profiles");
-    const q = query(postsRef);
-      await getDocs(q).then((snap)=> {
-         snapshots = snap.docs;
-         
-         snapshots.map((snapshot) => (
+  return {
+    props: {
+      userposts: posts,
+      userProfile: username,
+    },
+  };
+}
 
-           console.log('yooo')
+export async function getStaticPaths() {
+  let snapshots = [];
+  let username = "";
+  const postsRef = collection(db, "profiles");
+  const q = query(postsRef);
+  await getDocs(q).then((snap) => {
+    snapshots = snap.docs;
 
-         ))
-    })
-    
- 
-    return {
-      paths: snapshots.map((snapshot) => ({params:{username: snapshot.data().username }})),
-      fallback: false,
-    };
-  }
+    snapshots.map((snapshot) => console.log("yooo"));
+  });
+
+  return {
+    paths: snapshots.map((snapshot) => ({
+      params: { username: snapshot.data().username },
+    })),
+    fallback: false,
+  };
+}
